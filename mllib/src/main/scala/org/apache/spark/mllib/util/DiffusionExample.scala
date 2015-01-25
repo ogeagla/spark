af9b36d38186c13734c6ffbb6d81f247e61f8d81
Octavian Geagla <ogeagla@gmail.com>
2015-01-25 13:04:58 -0700

    wip 'working' iterations and test

--- mllib/src/main/scala/org/apache/spark/mllib/util/DiffusionExample.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/util/DiffusionExample.scala
@@ -1,37 +1,80 @@
 package org.apache.spark.mllib.util
 
-import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
+import org.slf4j.LoggerFactory
+
+import scala.collection.immutable
 
 
 class DiffusionExample  {
 
-  def runDiffusion(): Unit = {
 
-    val L = 150e3
-    val H = 100e3
-    val nx = 101
-    val ny = 51
-    val nt = 500
+  val log = LoggerFactory.getLogger(getClass)
 
-    val kappa = 1e-6
-    val dx = L / (nx - 1)
-    val dy = H / (ny - 1)
-    val dt = Math.pow(Math.min(dx, dy),  2) / kappa / 4.0;
-    val q = 0.0
-    val rho = 1e-4
-    val cp = 100
+  val L = 150e3
+  val H = 100e3
+  val nx = 101
+  val ny = 51
+  val nt = 500
+
+  val kappa = 1e-6
+  val dx = L / (nx - 1)
+  val dy = H / (ny - 1)
+  val dt = Math.pow(Math.min(dx, dy),  2) / kappa / 4.0;
+  val q = 0.0
+  val rho = 1e-4
+  val cp = 100
+
+  def runDiffusionOnce(m: IndexedSeq[IndexedSeq[Double]]): IndexedSeq[IndexedSeq[Double]] = {
 
     val diffTrans = new DiffusionTransformer(kappa, dt, dx, dy, q, rho, cp)
 
-    val phaseSpace = Array(
-      Array(0.0,1.0,4.0),
-      Array(2.0,3.0,4.0),
-      Array(4.2,4.2,1.3)
-    )
+    val maxY = m.size - 1
+    val maxX = m.head.size - 1
 
-    val inputVecForMiddleCell = Vectors.dense(Array(phaseSpace(1)(1), phaseSpace(1)(2), phaseSpace(1)(0), phaseSpace(2)(1), phaseSpace(0)(1)))
+    val updatedVals: immutable.IndexedSeq[immutable.IndexedSeq[Double]] = (0 to maxY) map {
+      y =>
+        (0 to maxX) map {
+          x =>
 
-    val newMiddleCell = diffTrans.transform(inputVecForMiddleCell)
+            if((x == 0) || (x == maxX) || (y == 0) || (y == maxY))
+              m(x)(y)
+            else
+              //to fully take advantage of spark, this should generate RDD[Vector], which we should fold over to get a RDD[(Vector)], which we should then transform and then somehow 'unpack'
+              diffTrans.transform(Vectors.dense(Array(m(x)(y), m(x)(y+1),m(x)(y-1),m(x+1)(y),m(x-1)(y))))(0)
+        }
+
+    }
+
+    updatedVals
+
+  }
+
+  def runDiffusionUntil(initM: IndexedSeq[IndexedSeq[Double]]): IndexedSeq[IndexedSeq[Double]] = {
+
+    val MAX = 100
+
+    var m1 = initM
+    var m2 = runDiffusionOnce(initM)
+    var iters = 1
+    while((! converged(m1, m2)) || (iters > MAX)) {
+      m1 = m2
+      m2 = runDiffusionOnce(m2)
+      iters = iters + 1
+    }
+    m2
+
+  }
+
+  def converged(m1: IndexedSeq[IndexedSeq[Double]], m2: IndexedSeq[IndexedSeq[Double]]): Boolean = {
+    def vectorsSameWithinTolerance(v1: Vector, v2: Vector, tol: Double): Boolean = {
+      if(v1.size != v2.size) {log.error(s"attempted to compare non-equal sized vectors: $v1 $v2"); return false}
+
+      (0 to v1.size-1).foreach(i => if( math.abs(v1.apply(i) - v2.apply(i)) > tol ) return false )
+      true
+    }
+    m1.zip(m2).map{case (v1, v2) => vectorsSameWithinTolerance(Vectors.dense(v1.toArray), Vectors.dense(v2.toArray), 1e-3)}.foldLeft(true)(_ && _)
+
   }
 
 }
--- mllib/src/test/scala/org/apache/spark/mllib/util/DiffusionExampleSuite.scala
+++ mllib/src/test/scala/org/apache/spark/mllib/util/DiffusionExampleSuite.scala
@@ -2,41 +2,89 @@ package org.apache.spark.mllib.util
 
 import org.apache.spark.mllib.linalg.Vectors
 import org.scalatest.FunSuite
-import org.apache.spark.mllib.util.MLlibTestSparkContext
 import org.apache.spark.mllib.util.TestingUtils._
+import org.apache.spark.mllib.rdd.RDDFunctions._
+import org.slf4j.LoggerFactory
+
+import scala.collection.immutable
+
 
 class DiffusionExampleSuite extends FunSuite with MLlibTestSparkContext  {
 
-  test("diffusion should work") {
+  val log = LoggerFactory.getLogger(getClass)
 
-    val L = 150e3
-    val H = 100e3
-    val nx = 101
-    val ny = 51
-    val nt = 500
 
-    val kappa = 1e-6
-    val dx = L / (nx - 1)
-    val dy = H / (ny - 1)
-    val dt = Math.pow(Math.min(dx, dy),  2) / kappa / 4.0;
-    val q = 0.0
-    val rho = 1e-4
-    val cp = 100
+  val L = 150e3
+  val H = 100e3
+  val nx = 101
+  val ny = 51
+  val nt = 500
+
+  val kappa = 1e-6
+  val dx = L / (nx - 1)
+  val dy = H / (ny - 1)
+  val dt = Math.pow(Math.min(dx, dy),  2) / kappa / 4.0;
+  val q = 0.0
+  val rho = 1e-4
+  val cp = 100
+
+  //phase space includes BCs, so this is once diffusion cell:
+  val M1 = Array(
+    Array(0.0,1.0,4.0),
+    Array(2.0,3.0,4.0),
+    Array(4.2,4.2,1.3)
+  )
+
+  val M2 = IndexedSeq(
+    IndexedSeq(1.0,1.0,1.0,1.0),
+    IndexedSeq(1.0,0.0,0.0,1.0),
+    IndexedSeq(1.0,0.0,0.0,1.0),
+    IndexedSeq(1.0,1.0,1.0,1.0)
+  )
+
+
+
+
+  test("diffusion should work for one middle cell") {
+
 
     val diffTrans = new DiffusionTransformer(kappa, dt, dx, dy, q, rho, cp)
 
-    val phaseSpace = Array(
-      Array(0.0,1.0,4.0),
-      Array(2.0,3.0,4.0),
-      Array(4.2,4.2,1.3)
-    )
-
-    val inputVecForMiddleCell = Vectors.dense(Array(phaseSpace(1)(1), phaseSpace(1)(2), phaseSpace(1)(0), phaseSpace(2)(1), phaseSpace(0)(1)))
+    val inputVecForMiddleCell = Vectors.dense(Array(M1(1)(1), M1(1)(2), M1(1)(0), M1(2)(1), M1(0)(1)))
 
     val newMiddleCell = diffTrans.transform(inputVecForMiddleCell)
 
-    assert(newMiddleCell ~== Vectors.dense(1.0) absTol 1e-3)
+    assert(newMiddleCell ~== Vectors.dense(2.8875) absTol 1e-3)
+    
+  }
+  
+  test("diffusion for a complete phase space") {
 
+    val diffTrans = new DiffusionTransformer(kappa, dt, dx, dy, q, rho, cp)
+
+    val maxY = M2.size - 1
+    val maxX = M2.head.size - 1
+
+    val updatedVals: immutable.IndexedSeq[immutable.IndexedSeq[Double]] = (0 to maxY) map {
+        y =>
+        (0 to maxX) map {
+          x =>
+
+            if((x == 0) || (x == maxX) || (y == 0) || (y == maxY))
+              M2(x)(y)
+            else
+              diffTrans.transform(Vectors.dense(Array(M2(x)(y), M2(x)(y+1),M2(x)(y-1),M2(x+1)(y),M2(x-1)(y))))(0)
+        }
+
+    }
+
+//    assert(updatedVals.toArray === Array(""))
+  }
+
+  test("runs until conv") {
+    val de = new DiffusionExample
+    val fin = de.runDiffusionUntil(M2)
+    fin.foreach(v => Vectors.dense(v.toArray) ~== Vectors.dense(1.0,1.0,1.0,1.0) absTol 1e-3)
   }
 
 }
